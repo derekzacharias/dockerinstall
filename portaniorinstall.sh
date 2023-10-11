aa42309a192a13a420c095490b6b18a80d9627f0
derekzacharias <derek_zacharias@hotmail.com>
2023-10-11 14:33:01 -0400

    Create portaniorinstall.sh

---
+++ portaniorinstall.sh
@@ -0,0 +1,19 @@
+#Install Portanior
+echo "Installing Portanior"
+
+#wait 5 sec
+sleep 5
+
+#Create a docker volume for persistent storage
+docker volume create portainer_data
+
+#download and install the Portainer Server container:
+
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+echo "Portanior install complete"
+
+echo "Click here to login" 
+https://docker0.test.intra:9443/
+https://docker1.test.intra:9443/
+https://docker2.test.intra:9443/
