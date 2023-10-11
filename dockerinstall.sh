3b9e71ac780d21656f6187aeb468cf6a5ba152a4
derekzacharias <derek_zacharias@hotmail.com>
2023-10-11 19:39:08 -0400

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -12,7 +12,7 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # Add Docker apt repository
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" 
 
 # Update the apt package index again
 sudo apt update
