1e11a216cbfb0bda69ee12a212e1cde4c8e79b35
derekzacharias <derek_zacharias@hotmail.com>
2023-10-15 09:50:38 -0400

    Update portaniorinstall.sh

--- portaniorinstall.sh
+++ portaniorinstall.sh
@@ -5,11 +5,11 @@ echo "Installing Portanior"
 sleep 5
 
 #Create a docker volume for persistent storage
-docker volume create portainer_data
+sudo docker volume create portainer_data
 
 #download and install the Portainer Server container:
 
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
 echo "Portanior install complete"
 
