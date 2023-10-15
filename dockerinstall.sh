d8df077c562cbb095bdf3676e98fb44b4e1c23ee
derekzacharias <derek_zacharias@hotmail.com>
2023-10-15 10:20:32 -0400

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -39,5 +39,32 @@ docker-compose --version
 
 echo "Docker and Docker Compose installation completed!"
 
+#Install Portanior
+echo "Installing Portanior............................................................................."
+
+#wait 5 sec
+sleep 5
+
+#Create a docker volume for persistent storage
+sudo docker volume create portainer_data
+
+#download and install the Portainer Server container:
+
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+echo "Portanior install complete"
+
+
+echo "To access web console go to the following location:" 
+
+# Obtain the first available IP address
+ip_address=$(hostname -I | awk '{print $1}')
+
+# Construct the URL
+url="https://$ip_address"
+
+# Open the URL in Google Chrome
+google-chrome "$url"
+
 
 
