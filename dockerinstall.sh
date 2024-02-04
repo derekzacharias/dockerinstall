462d2542adafe4cf77489f74396d00f9e4d11ed9
derekzacharias <derek_zacharias@hotmail.com>
2024-02-04 11:52:21 -0500

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -40,24 +40,24 @@ docker-compose --version
 echo "Docker and Docker Compose installation completed!"
 
 #Install Portanior
-echo "Installing Portanior............................................................................."
+#echo "Installing Portanior............................................................................."
 
 #wait 5 sec
-sleep 5
+#sleep 5
 
 #Create a docker volume for persistent storage
-sudo docker volume create portainer_data
+#sudo docker volume create portainer_data
 
 #download and install the Portainer Server container:
-sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+#sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
-echo "Portanior install complete"
+#echo "Portanior install complete"
 
-echo "To access Portanior web console go to the following location:" 
+#echo "To access Portanior web console go to the following location:" 
 
 # Link to Address
 
-echo "https://$(hostname -I | awk '{print $1}'):9443"
+#echo "https://$(hostname -I | awk '{print $1}'):9443"
 
 
 
